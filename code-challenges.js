365c6c9d19bea8b5636b486edee1c3d3434bc77e
dfletcher11 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-10 21:58:38 -0700

    adds instructor examples

--- code-challenges.js
+++ code-challenges.js
@@ -9,8 +9,8 @@
 // Describe your process:
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+//const fruit1 = "apple"
+//const fruit2 = "banana"
 // Expected outcome: "banana"
 
 // Set two:
@@ -20,9 +20,22 @@ const fruit2 = "banana"
 
 // --------------------1) Create the code that determines if a given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
-// Describe your process:
+//1> Describe your process:1>I determined to use conditional statements
+//2>created a variable of boilingPoint
+//3>declare the variable with the let command
+//4>console log the temperatures in the instructions.
 
-const temp = 42
+
+let boilingPoint = (42 , 350 , 212 )
+if (boilingPoint === 42) {
+    console.log("below boiling point")
+}else if (boilingPoint === 350) {
+    console.log("350 is above boiling point")
+}else if (boilingPoint === 212) {
+    //console.log("212 is at boiling point")
+}
+
+//const temp = 42
 // Expected output: "42 is below boiling point"
 
 // const temp = 350
@@ -31,38 +44,77 @@ const temp = 42
 // const temp = 212
 // Expected output: "212 is at boiling point"
 
-// --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
+ //--------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-// Describe your process:
+// Describe your process: 1.I created two variables for the two different arrays
+//2.I console log with the concat method to combine the arrays
+//3.I console log the first variable since they are combined and used the length method to show how many indexes their are in the array.
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
-// Expected output: 9
+let padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+let padres1998WorldSeriesRuns = [6, 3, 5, 3]
+console.log(padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns))
+console.log(padres1998WorldSeriesRuns.length)
+//const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+//const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+ //Expected output: 9
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-// Describe your process:
+ 
+ // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-const currentCohort = "Foxtrot 2022"
+ //1.created a variable named currentCohort
+ //2.declared the variable as an array foxtrot 2022
+ //3.console.log the variable with the reverse 
+
+let currentCohort = ["Foxtrot 2022"]
+ console.log(currentCohort.reverse)
+
+ // Describe your process:
+
+//const currentCohort = "Foxtrot 2022"
 // Expected output: "2202 tortxoF"
+
+
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
+let myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
+let givenValue1 = 42
+let givenValue2 = 10
+console.log(myNumbers.lastIndexOf(42))
+console.log(myNumbers.lastIndexOf(10))
+
+//1.created a variable mynumbers for array of integers
+//2.declared variable for two given numbers
+//3.console.log my variable of arrays and used lastindex function for given value of numbers.
+
 // Describe your process:
 
-const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
-const givenValue1 = 42
-// Expected output: 7
+//const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
+// const givenValue1 = 42
+// // Expected output: 7
 
-const givenValue2 = 10
-// Expected output: 8
+// const givenValue2 = 10
+// // Expected output: 8
 
-// --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
+let sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76 ]
+let sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+sanDiegoSummerTemperatures.sort()
+console.log(sanDiegoSummerTemperatures)
+sanDiegoWinterTemperatures.sort()
+console.log(sanDiegoWinterTemperatures)
 
-// Describe your process:
+//1.Created two variables and declared their value within a Array
+//2.Added the function sort to arrange temperature in order for for both arrays
+//3.console.log both variables
 
-const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
-// Expected output: [82, 80, 79, 77, 76, 73, 72]
 
-const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
-// Expected output: [68, 67, 66, 66, 62, 59, 59]
+// // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
+
+// // Describe your process:
+
+// const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+// // Expected output: [82, 80, 79, 77, 76, 73, 72]
+
+// const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+// // Expected output: [68, 67, 66, 66, 62, 59, 59]
--- coding-concepts.js
+++ coding-concepts.js
@@ -6,54 +6,54 @@
 // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+//const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+//console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:This will log indigo as the first color in the array of the other colors.
+// b) Verify and explain: the answer came back as 5. it did not push indigo into the array as the first index as the colors had a const variable.
 
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+ //console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will show the length of the variable which is 9
+// b) Verify and explain:the answer was 10, The length is the always the last index of the array plus one.
 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+//console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The greeting will show Hello with a capital O.
+// b) Verify and explain: It return just the letter o which was marked as only show the letter in the 4th index in the array.
 
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:it will only show the language ruby,it is the first index in the array
+// b) Verify and explain: the answer was ruby as it was the first index declared as a variable
 
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:It will capitalize both saturday and sunday
+// b) Verify and explain: I received a error stating "to" is not a function. the .toUpperCase is a string method being called on in a array and it will not work.
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will show the lenght of all the variables in the array which is 23
+// b) Verify and explain: it returned the variable named number.The length is the always the last index of the array plus one.
